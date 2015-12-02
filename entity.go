bd77d6a8a3097a38a399653c07eb3979be535d07
foreversmart <njutree@gmail.com>
2015-12-02 11:20:06 +0800

    change entity struct

--- entity.go
+++ entity.go
@@ -10,13 +10,13 @@ const (
 )
 
 type Entity struct {
-	Endpoint    string      `json:"endpoint"`
-	Tags        string      `json:"tags"`
-	Metric      string      `json:"metric"`
-	Value       float64     `json:"value"`
-	CounterType CounterType `json:"counterType"`
-	Timestamp   int64       `json:"timestamp"`
-	Step        int         `json:"step"`
+	Endpoint    string  `json:"endpoint"`
+	Tags        string  `json:"tags"`
+	Metric      string  `json:"metric"`
+	Value       float64 `json:"value"`
+	CounterType string  `json:"counterType"`
+	Timestamp   int64   `json:"timestamp"`
+	Step        int     `json:"step"`
 }
 
 func NewEntity(endpoint, tags, metric string) *Entity {
@@ -32,7 +32,7 @@ func NewEntity(endpoint, tags, metric string) *Entity {
 
 func (entity *Entity) SetValueAndType(value float64, counterType CounterType) {
 	entity.Value = value
-	entity.CounterType = counterType
+	entity.CounterType = counterType.String()
 }
 
 func (entity *Entity) SetStep(step int) {
